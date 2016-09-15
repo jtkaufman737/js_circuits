8da450496a582a606a7cd8dd1f50546c43203f5e
jtkaufman737 <jtkaufman737@gmail.com>
2016-09-15 16:20:45 -0400

    Adding notes

---
+++ U5/My_personal_notes.js
@@ -0,0 +1,13 @@
+// First up, let's create a new list
+// item and store it in a variable.
+var newListItem = document.createElement('li');
+
+// Alright, now let's update the
+//text content of that list item.
+newListItem.textContent = 'Feed the cat';
+
+// And finally, let's add that list
+//item as a child of the ul.
+document.getElementsByTagName('ul')[0].appendChild(newListItem);
+document.getElementsByTagName('ul')[0].appendChild(newListItem);
+document.getElementById('important').className = 'highlight';
