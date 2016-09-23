123308b3154adb24ad377d86f9868ddf5aee8622
jtkaufman737 <jtkaufman737@gmail.com>
2016-09-23 12:34:43 -0400

    Adding changes

---
+++ U5/to_do_list/main.js
@@ -0,0 +1,23 @@
+function addItem() {
+  var input = document.getElementsByTagName('input')[0];
+  var newItemText = input.value;
+  input.value = '';
+
+  var newItem = document.createElement('li');
+  newItem.innerHTML = newItemText;
+  document.getElementById('todoList').appendChild(newItem);
+  newItem.addEventListener('click',completeItem);
+}
+
+function completeItem() {
+  removeItemFromTodos(this);
+  addItemToCompleted(this);
+}
+
+function removeItemFromTodos() {
+}
+
+function addItemToCompleted() {
+}
+
+document.getElementsByTagName('button')[0].addEventListener('click', addItem);
