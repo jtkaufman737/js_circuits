1107d757458c40ee1e0b2d61c1076e1374447f0e
jtkaufman737 <jtkaufman737@gmail.com>
2016-09-24 10:02:28 -0400

    more dom traversal

---
+++ U6/atom_exercise/js/main.js
@@ -0,0 +1,18 @@
+$('h1').html('Hello from js!');
+
+$(function() {
+/* TRAVERSING THE DOM EXAMPLES
+    $('#thumbnails').children('div');
+
+    $('#thumbnails').children('div').children('img');
+
+    $('#thumbnails').find('.thumbnail');
+
+    $('#thumbnails').children();
+
+    $('.thumbnail').parent().parent('#thumbnails');
+
+
+    $('.thumbnail').parent('div').next().children('.thumbnail');
+    $('#thumbnails div:last-child').prev(); */
+});
